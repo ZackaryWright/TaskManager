d839697a3e8e17c0a3522d074d3c3aa99d7bde09
ZackaryWright <47463747+ZackaryWright@users.noreply.github.com>
2023-03-10 12:37:07 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
+
+//React.FC = Functional component for React
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}])
+
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
+  return (
+    <div className="App">
+      <span className='heading'>Task Manager</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
+    </div>
+  );
+}
+
+export default App;
